ac4ce982ba6ff112e621c80527c9da5f35f116f9
Bobrovskikh <bobrovskikh@yandex-team.ru>
2019-02-22 11:54:34 +0300

    minor changes :)

--- js/background.js
+++ js/background.js
@@ -2,18 +2,18 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.runtime.openOptionsPage();
 });
 
-let width = 260;
-let height = 500;
-let wd = screen.width/2 - width/2;
-let tp = screen.height/2 - height/2;
+const width = 260;
+const height = 500;
+const left = (screen.width - width) / 2;
+const top = (screen.height - height) / 2;
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function() {
   chrome.windows.create({
     url: chrome.runtime.getURL("../popup.html"),
     type: "popup",
-    width: width,
-    height: height,
-    left:  wd,
-    top: tp
+    width,
+    height,
+    left,
+    top,
   });
 });
